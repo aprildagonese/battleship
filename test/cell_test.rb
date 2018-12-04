dfece800791d616968730799eeb905fe245ae756
nagerz <znager@gmail.com>
2018-12-04 16:08:05 -0700

    No idea. Making me commit but can;t see any changes

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -43,4 +43,29 @@ class CellTest < Minitest::Test
     assert_equal true, cell.fired_upon?
   end
 
+  def test_fired_upon_no_ship
+    cell = Cell.new("B4")
+    assert_equal false, cell.fired_upon?
+    cell.fire_upon
+    assert_equal true, cell.fired_upon?
+  end
+
+  def test_render
+    cell_1 = Cell.new("B4")
+    assert_equal ".", cell_1.render
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    assert_equal "S", cell_2.render(true)
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
+    assert_equal "X", cell_2.render
+  end
+
 end
