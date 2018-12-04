0d1a5cd175adc7b6988616dafefc639e0bec68d5
April Dagonese <adagonese@gmail.com>
2018-12-04 07:05:08 -0700

    adds Cell class and tests

---
+++ test/cell_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/test'
+require './lib/ship'
+require './lib/cell'
+
+class TestCell < Minitest::Test
+
+  def test_it_exists
+    cell = Cell.new("B4")
+    assert_instance_of Cell, cell
+  end
+
+  def test_retrieve_coordinate
+    cell = Cell.new("B4")
+    assert_equal "B4", cell.coordinate
+  end
+
+  def test_empty_at_initialization
+    cell = Cell.new("B4")
+    assert_equal true, cell.empty?
+  end
+
+  def test_ship_in_cell
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal cruiser, cell.ship
+  end
+
+  def test_cell_no_longer_empty
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal false, cell.empty?
+  end
+
+end
