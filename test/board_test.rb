d71aaae5087a1380b2bce774ddf676645b63c10a
April Dagonese <adagonese@gmail.com>
2018-12-12 14:56:36 -0700

    Added fix for accepting coords that weren't on the board

--- test/board_test.rb
+++ test/board_test.rb
@@ -17,12 +17,6 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, @board
   end
 
-  def test_it_gets_ships
-    board2 = Board.new(6)
-    assert_instance_of Ship, board2.get_ships.first
-    assert_equal 2, board2.get_ships.count
-  end
-
   def test_cells_hash
     assert_equal Hash, @board.cells.class
     assert_equal 16, @board.cells.length
@@ -51,6 +45,11 @@ class BoardTest < Minitest::Test
   #   assert_equal false, board2.valid_coordinate?("E8")
   #   assert_equal false, board2.valid_coordinate?("Z77")
   # end
+
+  def test_invalid_placement_coords_not_on_board
+    assert_equal false, @board.valid_placement?(@cruiser,["Z1", "Z2"])
+    assert_equal false, @board.valid_placement?(@submarine,["A22", "A23", "A24"])
+  end
 
   def test_invalid_placement_not_correct_length
     assert_equal false, @board.valid_placement?(@cruiser,["A1", "A2"])
@@ -127,6 +126,14 @@ class BoardTest < Minitest::Test
     hit_cell2.fire_upon
     puts @submarine.health
 
+  end
+
+  def test_it_knows_coordinates_are_on_board
+    board2 = Board.new(4)
+
+    assert_equal true, board2.all_coordinates_on_board?(["A1", "A2", "A3"])
+    assert_equal false, board2.all_coordinates_on_board?(["Z1", "A2", "A3"])
+    assert_equal false, board2.all_coordinates_on_board?(["A10", "A11", "A12"])
   end
 
 end
